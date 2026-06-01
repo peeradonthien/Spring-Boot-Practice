2930931c91bd635eb5747c0a28d72075ac78c150
Peeradon <peeradonthienpongkasem@gmail.com>
2026-06-01 18:37:27 +0700

    finish CRUD

--- crudapp/src/main/java/com/chelseapeeradon/crudapp/CrudappApplication.java
+++ crudapp/src/main/java/com/chelseapeeradon/crudapp/CrudappApplication.java
@@ -1,10 +1,13 @@
 package com.chelseapeeradon.crudapp;
 
+import com.chelseapeeradon.crudapp.entity.Person;
+import com.chelseapeeradon.crudapp.repository.PersonDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import java.util.List;
 
 @SpringBootApplication
 public class CrudappApplication {
@@ -13,9 +16,49 @@ public class CrudappApplication {
 		SpringApplication.run(CrudappApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(PersonDAO dao) {
 		return runner->{
-			System.out.println("Hello");
+//			saveData(dao);
+//			deleteData(dao);
+//			getData(dao);
+//			getAllData(dao);
+			updateData(dao);
 		};
+	}
+
+	public void saveData(PersonDAO dao) {
+		Person obj1 = new Person("sadsad","dd");
+		dao.save(obj1);
+		System.out.println("Insert Complete");
+	}
+
+	public void deleteData(PersonDAO dao) {
+		int id = 1;
+		dao.delete(id);
+		System.out.println("Delete Complete");
+	}
+
+	public void getData(PersonDAO dao) {
+		int id = 1;
+		Person person = dao.get(id);
+		System.out.println("Get Complete");
+		System.out.println(person);
+	}
+
+	public void getAllData(PersonDAO dao) {
+		List<Person> data = dao.getAll();
+		for(Person person : data) {
+			System.out.println(person);
+		}
+
+	}
+
+	public void updateData(PersonDAO dao) {
+		int id = 4;
+		Person person = dao.get(id);
+		person.setFname("JJ");
+		person.setLname("KK");
+		dao.update(person);
+		System.out.println("Update Complete");
 	}
 }
